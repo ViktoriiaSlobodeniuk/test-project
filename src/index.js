585c9d985f1d1e75a2563f76f316aa93379ba64b
Viktoriia Slobodeniuk <vikavas988@gmail.com>
2023-04-26 23:41:32 +0300

    +

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter
-  // basename="/test-project"
-  >
+  <BrowserRouter basename="/test-project">
     <React.StrictMode>
       <App />
     </React.StrictMode>
